572004f4a0ead2401fb37d7690e41736a0a6db7d
Wishwa8764 <wishwaaloka89764@gmail.com>
2026-08-10 23:44:03 +0530

    Day 1 - Istarted

---
+++ script.js
@@ -0,0 +1,10 @@
+// Day 1 - Basics
+let myGoal = "Software Engineer in 6 months";
+let hoursPerDay = 4;
+console.log("My Goal:", myGoal);
+
+if (hoursPerDay >= 3) {
+  console.log("You will make it.");
+} else {
+  console.log("Increase your hours.");
+}
